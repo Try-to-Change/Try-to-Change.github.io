13ef012b4bf61bf3e9a9ae881391eb8f6dd8d2cf
try-to-change <1843851200@qq.com>
2024-12-10 18:05:33 +0800

    Site updated: 2024-12-10 18:05:28

--- js/dist/search.dev.js
+++ js/dist/search.dev.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // A local search script with the help of
 // [hexo-generator-search](https://github.com/PaicHyperionDev/hexo-generator-search)
 // Copyright (C) 2015
@@ -22,8 +24,7 @@
 // Pieter Robberechts <http://github.com/probberechts>
 
 /*exported searchFunc*/
-var searchFunc = function(path, searchId, contentId) {
-
+var searchFunc = function searchFunc(path, searchId, contentId) {
   function stripHtml(html) {
     html = html.replace(/<style([\s\S]*?)<\/style>/gi, "");
     html = html.replace(/<script([\s\S]*?)<\/script>/gi, "");
@@ -39,43 +40,52 @@ var searchFunc = function(path, searchId, contentId) {
   }
 
   function getAllCombinations(keywords) {
-    var i, j, result = [];
+    var i,
+        j,
+        result = [];
 
     for (i = 0; i < keywords.length; i++) {
-        for (j = i + 1; j < keywords.length + 1; j++) {
-            result.push(keywords.slice(i, j).join(" "));
-        }
+      for (j = i + 1; j < keywords.length + 1; j++) {
+        result.push(keywords.slice(i, j).join(" "));
+      }
     }
+
     return result;
   }
 
   $.ajax({
     url: path,
     dataType: "xml",
-    success: function(xmlResponse) {
+    success: function success(xmlResponse) {
       // get the contents from search data
-      var datas = $("entry", xmlResponse).map(function() {
+      var datas = $("entry", xmlResponse).map(function () {
         var linkElement = $("link", this);
         return {
-            title: $("title", this).text(),
-            content: $("content", this).text(),
-            url: linkElement.attr("href")
+          title: $("title", this).text(),
+          content: $("content", this).text(),
+          url: linkElement.attr("href")
         };
-    }).get();
+      }).get();
       var $input = document.getElementById(searchId);
-      if (!$input) { return; }
-      var $resultContent = document.getElementById(contentId);
 
-      $input.addEventListener("input", function(){
+      if (!$input) {
+        return;
+      }
+
+      var $resultContent = document.getElementById(contentId);
+      $input.addEventListener("input", function () {
         var resultList = [];
-        var keywords = getAllCombinations(this.value.trim().toLowerCase().split(" "))
-          .sort(function(a,b) { return b.split(" ").length - a.split(" ").length; });
+        var keywords = getAllCombinations(this.value.trim().toLowerCase().split(" ")).sort(function (a, b) {
+          return b.split(" ").length - a.split(" ").length;
+        });
         $resultContent.innerHTML = "";
+
         if (this.value.trim().length <= 0) {
           return;
-        }
-        // perform local searching
-        datas.forEach(function(data) {
+        } // perform local searching
+
+
+        datas.forEach(function (data) {
           var matches = 0;
           var dataTitle = data.title.trim().toLowerCase();
           var dataTitleLowerCase = dataTitle.toLowerCase();
@@ -84,68 +94,74 @@ var searchFunc = function(path, searchId, contentId) {
           var dataUrl = data.url;
           var indexTitle = -1;
           var indexContent = -1;
-          var firstOccur = -1;
-          // only match artiles with not empty contents
+          var firstOccur = -1; // only match artiles with not empty contents
+
           if (dataContent !== "") {
-            keywords.forEach(function(keyword) {
+            keywords.forEach(function (keyword) {
               indexTitle = dataTitleLowerCase.indexOf(keyword);
               indexContent = dataContentLowerCase.indexOf(keyword);
 
-              if( indexTitle >= 0 || indexContent >= 0 ){
+              if (indexTitle >= 0 || indexContent >= 0) {
                 matches += 1;
+
                 if (indexContent < 0) {
                   indexContent = 0;
                 }
+
                 if (firstOccur < 0) {
                   firstOccur = indexContent;
                 }
               }
             });
-          }
-          // show search results
+          } // show search results
+
+
           if (matches > 0) {
             var searchResult = {};
             searchResult.rank = matches;
-            searchResult.str = "<li><a href='"+ dataUrl +"' class='search-result-title'>"+ dataTitle +"</a>";
+            searchResult.str = "<li><a href='" + dataUrl + "' class='search-result-title'>" + dataTitle + "</a>";
+
             if (firstOccur >= 0) {
               // cut out 100 characters
               var start = firstOccur - 20;
               var end = firstOccur + 80;
 
-              if(start < 0){
+              if (start < 0) {
                 start = 0;
               }
 
-              if(start == 0){
+              if (start == 0) {
                 end = 100;
               }
 
-              if(end > dataContent.length){
+              if (end > dataContent.length) {
                 end = dataContent.length;
               }
 
-              var matchContent = dataContent.substring(start, end);
+              var matchContent = dataContent.substring(start, end); // highlight all keywords
 
-              // highlight all keywords
               var regS = new RegExp(keywords.join("|"), "gi");
-              matchContent = matchContent.replace(regS, function(keyword) {
-                return "<em class=\"search-keyword\">"+keyword+"</em>";
+              matchContent = matchContent.replace(regS, function (keyword) {
+                return "<em class=\"search-keyword\">" + keyword + "</em>";
               });
-
-              searchResult.str += "<p class=\"search-result\">" + matchContent +"...</p>";
+              searchResult.str += "<p class=\"search-result\">" + matchContent + "...</p>";
             }
+
             searchResult.str += "</li>";
             resultList.push(searchResult);
           }
         });
+
         if (resultList.length) {
-          resultList.sort(function(a, b) {
-              return b.rank - a.rank;
+          resultList.sort(function (a, b) {
+            return b.rank - a.rank;
           });
-          var result ="<ul class=\"search-result-list\">";
+          var result = "<ul class=\"search-result-list\">";
+
           for (var i = 0; i < resultList.length; i++) {
             result += resultList[i].str;
           }
+
           result += "</ul>";
           $resultContent.innerHTML = result;
         }
